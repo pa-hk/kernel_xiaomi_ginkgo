8f6909a8c06d0af5bef824973baa1edc6e951ed5
ZorEl212 <yabsirad212@proton.me>
2024-10-11 15:02:49 +0000

    Revert "sched/uclamp: Create a new uclamp.boosted tunable"
    This reverts commit 90db26177314b2842d45ab59f65b968f903dedb9.
    
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1870,10 +1870,6 @@ int cpu_uclamp_ls_write_u64(struct cgroup_subsys_state *css,
                               struct cftype *cftype, u64 ls);
 u64 cpu_uclamp_ls_read_u64(struct cgroup_subsys_state *css,
                              struct cftype *cft);
-int cpu_uclamp_boost_write_u64(struct cgroup_subsys_state *css,
-                              struct cftype *cftype, u64 boost);
-u64 cpu_uclamp_boost_read_u64(struct cgroup_subsys_state *css,
-                             struct cftype *cft);
 
 #if !defined(CONFIG_SCHED_TUNE)
 static u64 st_boost_read(struct cgroup_subsys_state *css,
@@ -2035,12 +2031,6 @@ static struct cftype files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_uclamp_ls_read_u64,
 		.write_u64 = cpu_uclamp_ls_write_u64,
-	},
-	{
-		.name = "uclamp.boosted",
-		.flags = CFTYPE_NOT_ON_ROOT,
-		.read_u64 = cpu_uclamp_boost_read_u64,
-		.write_u64 = cpu_uclamp_boost_write_u64,
 	},
 #if !defined(CONFIG_SCHED_TUNE)
 	{
